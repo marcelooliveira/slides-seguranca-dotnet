220f129c2fffc7be27e7276daaa6061fe57b7600
marcelooliveira <mclricardo@gmail.com>
2024-12-23 22:09:22 -0300

    Desfazendo 'Validando dados' (Vídeo 4.1)

--- MedVoll/MedVoll.Web/Controllers/MedicoController.cs
+++ MedVoll/MedVoll.Web/Controllers/MedicoController.cs
@@ -54,11 +54,6 @@ namespace MedVoll.Web.Controllers
                 return Redirect("/medicos");
             }
 
-            if (!ModelState.IsValid)
-            {
-                return View(PaginaCadastro, dados);
-            }
-
             try
             {
                 await _service.CadastrarAsync(dados);
