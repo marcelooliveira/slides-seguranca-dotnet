5774ba56be965d14da44db75527aa3ba18ca832e
marcelooliveira <mclricardo@gmail.com>
2025-01-02 13:36:51 -0300

    Vídeo 4.2 - Sanitizando dados

--- MedVoll/MedVoll.Web/Program.cs
+++ MedVoll/MedVoll.Web/Program.cs
@@ -130,4 +130,15 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+// Middleware para adicionar cabeçalhos de segurança.
+app.Use(async (context, next) =>
+{
+    // Restringe fontes de conteúdo para evitar XSS.
+    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self';");
+
+    // Previne a interpretação incorreta de MIME types.
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    await next();
+});
+
 app.Run();
