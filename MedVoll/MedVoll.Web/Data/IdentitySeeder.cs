23d977b4a9b45dd8c64947607708d6db87a53f54
marcelooliveira <mclricardo@gmail.com>
2025-01-02 16:53:42 -0300

    Vídeo 5.2 - Dados de usuário com claims

--- MedVoll/MedVoll.Web/Data/IdentitySeeder.cs
+++ MedVoll/MedVoll.Web/Data/IdentitySeeder.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace MedVoll.Web.Data
 {
@@ -33,6 +34,18 @@ namespace MedVoll.Web.Data
             {
                 await userManager.AddToRoleAsync(alice, adminRole);
             }
+
+            IdentityUser? bob = await userManager.FindByEmailAsync("bob@smith.com");
+
+            //Adiciona claims
+            IList<Claim> userClaims = await userManager.GetClaimsAsync(alice);
+            await userManager.RemoveClaimsAsync(alice, userClaims);
+            await userManager.AddClaimAsync(alice, new Claim("FullName", "Alice Smith"));
+            await userManager.AddClaimAsync(alice, new Claim("Role", "Admin"));
+
+            userClaims = await userManager.GetClaimsAsync(bob);
+            await userManager.RemoveClaimsAsync(bob, userClaims);
+            await userManager.AddClaimAsync(bob, new Claim("FullName", "Bob Smith"));
         }
         private static async Task CreateUserAsync(UserManager<IdentityUser> userManager, string email, string password, string role)
         {
